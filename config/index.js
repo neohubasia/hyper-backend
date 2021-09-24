706680184c8acfc402f9fc26a30996293a22f0b6
waiphyo285 <waiphyoenaing.joy007@gmail.com>
2021-09-24 15:10:45 +0630

    filter design for all list page

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV,
   PORT: process.env.PORT,
   app: {
-    NAME: "itemplate | admin",
+    NAME: "hyper dashboard",
     FILE: "itemplate-file",
     DESC: "business  solution for general purpose",
     icons: {
