0cb1c345922c43f4278989bf7b6cf87a0be3435f
waiphyo285 <waiphyoenaing.joy007@gmail.com>
2021-09-26 19:50:30 +0630

    web push testing

---
+++ drivers/webserver/routes/wp_api/web_push.js
@@ -0,0 +1,18 @@
+var wp = require('../../../../librarys/web-push');
+
+let web_push = module.exports = {};
+
+web_push.index = (req, res, next) => {
+    const subscription = req.body;
+    const payload = JSON.stringify({
+        title: 'hyper marketplace',
+        body: "New order is pending to you. Please confirm this order.",
+        icon: "http://mongoosejs.com/docs/images/mongoose5_62x30_transparent.png"
+    });
+
+    res.status(201).json({});
+
+    wp.sendNotification(subscription, payload).catch(error => {
+        console.error(error.stack);
+    });
+}
