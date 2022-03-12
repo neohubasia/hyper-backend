a9fa37dcf2b688b6a3dab71b37f4bfa0849b2e4c
Min Zin Phyo <minzinphyo143@gmail.com>
2022-03-12 11:06:41 +0630

    check error

--- app.js
+++ app.js
@@ -130,20 +130,20 @@ passport.serializeUser(UserModel.serializeUser());
 passport.deserializeUser(UserModel.deserializeUser());
 
 // error handler
-app.use(function (err, req, res, next) {
-  console.log("Error found ", err);
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+// app.use(function (err, req, res, next) {
+//   console.log("Error found ", err);
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
+//   // render the error page
+//   res.status(err.status || 500);
 
-  // res.json({
-  //   "status": "ERR",
-  //   "message": err
-  // })
-  res.sendFile("./views/404/index.html", { root: __dirname });
-});
+//   // res.json({
+//   //   "status": "ERR",
+//   //   "message": err
+//   // })
+//   res.sendFile("./views/404/index.html", { root: __dirname });
+// });
 
 module.exports = app;
